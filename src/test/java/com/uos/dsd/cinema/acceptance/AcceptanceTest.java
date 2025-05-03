cec0e3e6b31012d683641854db5b8ad059b932c5
김혜주 <sueelly@naver.com>
2025-05-03 19:34:41 +0900

    feat(AcceptanceTest): Isolate each acceptance test methods by using dbinitializer
    Related to #3

--- src/test/java/com/uos/dsd/cinema/acceptance/AcceptanceTest.java
+++ src/test/java/com/uos/dsd/cinema/acceptance/AcceptanceTest.java
@@ -1,14 +1,18 @@
 package com.uos.dsd.cinema.acceptance;
 
-import com.uos.dsd.cinema.TestcontainersConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.AfterEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.uos.dsd.cinema.utils.DBInitializer;
+import com.uos.dsd.cinema.TestcontainersConfiguration;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,8 +21,17 @@ import org.slf4j.LoggerFactory;
     mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
 )
 @Testcontainers
-@Import(TestcontainersConfiguration.class)
+@Import({DBInitializer.class, TestcontainersConfiguration.class})
 public abstract class AcceptanceTest {
 
+    @Autowired
+    DBInitializer dbInitializer;
+
     protected static final Logger log = LoggerFactory.getLogger(AcceptanceTest.class);
+
+    @AfterEach
+    public void clear() {
+
+        dbInitializer.clear();
+    }
 }

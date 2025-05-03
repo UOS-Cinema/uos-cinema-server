8f06f82ee3ef798f1a1870f8024413db712dda00
김혜주 <sueelly@naver.com>
2025-05-04 00:33:20 +0900

    refactor(DBInitializer): Apply strategy pattern so that query which is belong to specific db is hidden
    Related to #3

---
+++ src/test/java/com/uos/dsd/cinema/utils/DBInitializeStrategy.java
@@ -0,0 +1,18 @@
+package com.uos.dsd.cinema.utils;
+
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public interface DBInitializeStrategy {
+
+    void truncateTables(List<String> tableNames);
+
+    void setForeignKeyCheck(boolean check);
+
+    void createTable();
+
+    void createData();
+
+    void setEntityManager(EntityManager entityManager);
+}

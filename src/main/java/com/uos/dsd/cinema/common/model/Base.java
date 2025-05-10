94a329f0382210c2bd35c211220e675600d74132
ikjeong <jipark7937@gmail.com>
2025-05-11 02:58:24 +0900

    feat(Base): Add annotation for jpa auditing

--- src/main/java/com/uos/dsd/cinema/common/model/Base.java
+++ src/main/java/com/uos/dsd/cinema/common/model/Base.java
@@ -1,17 +1,31 @@
 package com.uos.dsd.cinema.common.model;
 
-import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Getter;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
 @Getter
+@MappedSuperclass
 public abstract class Base {
 
+    @CreatedDate
+    @Column(updatable = false)
     protected LocalDateTime createdAt;
+    @LastModifiedDate
     protected LocalDateTime updatedAt;
     protected LocalDateTime deletedAt;
 
     public void delete() {
         this.deletedAt = LocalDateTime.now();
+    }
+
+    public boolean isDeleted() {
+        return deletedAt != null;
     }
 }

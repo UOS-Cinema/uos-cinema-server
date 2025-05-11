578ee12fab8991766c2395af00b12f219ceb4759
ikjeong <jipark7937@gmail.com>
2025-05-12 03:43:01 +0900

    feat(Admin): Implement password hashing and validation

--- src/main/java/com/uos/dsd/cinema/application/service/AuthService.java
+++ src/main/java/com/uos/dsd/cinema/application/service/AuthService.java
@@ -1,14 +1,13 @@
 package com.uos.dsd.cinema.application.service;
 
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.uos.dsd.cinema.application.port.out.repository.AdminRepository;
 import com.uos.dsd.cinema.common.exception.code.CommonResultCode;
 import com.uos.dsd.cinema.common.exception.http.BadRequestException;
 import com.uos.dsd.cinema.common.exception.http.UnauthorizedException;
 import com.uos.dsd.cinema.domain.model.Admin;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +22,11 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final PasswordEncoder passwordEncoder;
     private final AdminRepository adminRepository;
 
     public Long registerAdmin(String name, String password) {
 
-        Admin admin = new Admin(name, passwordEncoder.encode(password));
+        Admin admin = new Admin(name, password);
         try {
             return adminRepository.save(admin).getId();
         } catch (DataIntegrityViolationException e) {
@@ -40,7 +38,7 @@ public class AuthService {
     public Admin loginAdmin(String name, String password) {
 
         Optional<Admin> admin = adminRepository.findByName(name);
-        if (admin.isEmpty() || !admin.get().isPasswordMatched(passwordEncoder.encode(password))) {
+        if (admin.isEmpty() || !admin.get().isPasswordMatched(password)) {
             throw new UnauthorizedException(CommonResultCode.UNAUTHORIZED);
         }
         return admin.get();

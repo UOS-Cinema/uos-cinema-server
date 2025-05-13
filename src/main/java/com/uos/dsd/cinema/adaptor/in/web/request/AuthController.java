57a8077ff154ae9764a3a4e848ac575fa1929184
ikjeong <jipark7937@gmail.com>
2025-05-14 03:28:41 +0900

    feat:(AuthInfra): Implement controller and repository for auth

---
+++ src/main/java/com/uos/dsd/cinema/adaptor/in/web/request/AuthController.java
@@ -0,0 +1,68 @@
+package com.uos.dsd.cinema.adaptor.in.web.request;
+
+import com.uos.dsd.cinema.application.service.AuthService;
+import com.uos.dsd.cinema.common.response.ApiResponse;
+import com.uos.dsd.cinema.domain.model.Admin;
+import com.uos.dsd.cinema.common.exception.http.BadRequestException;
+import com.uos.dsd.cinema.common.exception.code.CommonResultCode;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    
+    private final AuthService authService;
+    
+    @PostMapping("/admin/signup")
+    public ApiResponse<AdminSignupResponse> signup(@RequestBody AdminSignupRequest request) {
+
+        authService.signupAdmin(request.name(), request.password());
+        return ApiResponse.success(new AdminSignupResponse(request.name()));
+    }
+
+    @PostMapping("/admin/login")
+    public ApiResponse<AdminLoginResponse> login(@RequestBody AdminLoginRequest request) {
+
+        Admin admin = authService.loginAdmin(request.name(), request.password());
+
+        // TODO: Generate JWT token and return it in the response
+
+        return ApiResponse.success(new AdminLoginResponse(admin.getName()));
+    }
+    
+    // DTOs
+    public record AdminLoginRequest(String name, String password) {
+
+        public AdminLoginRequest {
+
+            if (name == null || name.isBlank()) {
+                throw new BadRequestException(CommonResultCode.BAD_REQUEST, "Name cannot be null or blank");
+            }
+            if (password == null || password.isBlank()) {
+                throw new BadRequestException(CommonResultCode.BAD_REQUEST, "Password cannot be null or blank");
+            }
+        }
+    }
+    public record AdminSignupRequest(String name, String password) {
+
+        public AdminSignupRequest {
+
+            if (name == null || name.isBlank()) {
+                throw new BadRequestException(CommonResultCode.BAD_REQUEST, "Name cannot be null or blank");
+            }
+            if (password == null || password.isBlank()) {
+                throw new BadRequestException(CommonResultCode.BAD_REQUEST, "Password cannot be null or blank");
+            }
+        }
+    }
+
+    public record AdminLoginResponse(String name) { }
+
+    public record AdminSignupResponse(String name) { }
+
+}

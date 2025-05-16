e7f9f3d5bbbaede583c50eb1ac76c8db59cf5553
ikjeong <jipark7937@gmail.com>
2025-05-16 17:25:39 +0900

    refactor(JpaAdminRepository): Change JpaRepository to CrudRepository

--- src/main/java/com/uos/dsd/cinema/adaptor/out/persistence/jpa/JpaAdminRepository.java
+++ src/main/java/com/uos/dsd/cinema/adaptor/out/persistence/jpa/JpaAdminRepository.java
@@ -3,8 +3,8 @@ package com.uos.dsd.cinema.adaptor.out.persistence.jpa;
 import com.uos.dsd.cinema.application.port.out.repository.admin.AdminRepository;
 import com.uos.dsd.cinema.domain.admin.Admin;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface JpaAdminRepository extends JpaRepository<Admin, Long>, AdminRepository {
+public interface JpaAdminRepository extends CrudRepository<Admin, Long>, AdminRepository {
 
 }

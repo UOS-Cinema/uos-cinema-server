2a058233ba04f56ab8fc4743c8ed3ab476462ab9
김혜주 <sueelly@naver.com>
2025-05-23 20:30:03 +0900

    feat(Theater): Implement theater service with test

---
+++ src/main/java/com/uos/dsd/cinema/application/service/TheaterService.java
@@ -0,0 +1,112 @@
+package com.uos.dsd.cinema.application.service;
+
+import org.springframework.stereotype.Service;
+
+import com.uos.dsd.cinema.application.port.in.theater.usecase.CreateTheaterUseCase;
+import com.uos.dsd.cinema.application.port.in.theater.usecase.ReadTheaterUseCase;
+import com.uos.dsd.cinema.application.port.out.theater.TheaterRepository;
+import com.uos.dsd.cinema.application.port.in.theater.usecase.ModifyTheaterUseCase;
+import com.uos.dsd.cinema.application.port.in.theater.usecase.DeleteTheaterUseCase;
+import com.uos.dsd.cinema.application.port.in.theater.command.CreateTheaterCommand;
+import com.uos.dsd.cinema.application.port.in.theater.command.ModifyTheaterCommand;
+import com.uos.dsd.cinema.adaptor.in.web.theater.response.TheaterResponse;
+import com.uos.dsd.cinema.domain.screen_type.ScreenType;
+import com.uos.dsd.cinema.domain.theater.Theater;
+import com.uos.dsd.cinema.domain.theater.exception.TheaterExceptionCode;
+import com.uos.dsd.cinema.common.exception.http.BadRequestException;
+import com.uos.dsd.cinema.common.exception.http.NotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.hibernate.exception.ConstraintViolationException;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TheaterService implements
+        CreateTheaterUseCase,
+        ReadTheaterUseCase, 
+        ModifyTheaterUseCase, 
+        DeleteTheaterUseCase {
+
+    private final TheaterRepository theaterRepository;
+
+    @Override
+    public Long createTheater(CreateTheaterCommand command) {
+        
+        Theater theater = new Theater(
+            command.number(),
+            command.name(),
+            command.layout(),
+            command.screenTypes()
+        );
+
+        try {
+            theaterRepository.save(theater);
+            return theater.getNumber();
+        } catch (ConstraintViolationException e) {
+            throw new BadRequestException(
+                TheaterExceptionCode.THEATER_ALREADY_EXISTS,
+                String.format(TheaterExceptionCode.THEATER_ALREADY_EXISTS.getMessage(), 
+                command.number(), command.name()));
+        }
+    }
+
+    @Override
+    public TheaterResponse readTheater(Long theaterNumber) {
+
+        Theater theater = findByNumber(theaterNumber);
+
+        return new TheaterResponse(
+            theater.getNumber(),
+            theater.getName(),
+            theater.getLayout(),
+            theater.getScreenTypes().stream()
+                .map(ScreenType::getType)
+                .collect(Collectors.toList())
+        );
+    }
+
+    @Override
+    public Long modifyTheater(ModifyTheaterCommand command) {
+
+        Theater theater = findByNumber(command.number());
+
+        theater.modifyName(command.name());
+        if (theater.getLayout() != command.layout()) {
+            // TODO: 해당 상영관에 예매한 사람이 없는 경우에만 변경 가능
+            theater.deleteSeats();
+            theaterRepository.saveAndFlush(theater);
+            theater.modifyLayout(command.layout());
+        }
+        if (theater.getScreenTypes() != command.screenTypes()) {
+            // TODO: 해당 상영관을 예매한 사람이 없는 경우에만 변경 가능
+            theater.modifyScreenTypes(command.screenTypes());
+        }
+        theaterRepository.save(theater);
+        return theater.getNumber();
+    }
+
+    @Override
+    public void deleteTheater(Long theaterNumber) {
+
+        // TODO: 해당 상영관의 상영 예정 예매 내역이 없는 경우에만 삭제 가능
+        try {
+            theaterRepository.deleteById(theaterNumber);
+        } catch (EmptyResultDataAccessException e) {
+            throw new RuntimeException("Theater not found: " + theaterNumber);
+        }
+    }
+
+    private Theater findByNumber(Long theaterNumber) {
+        return theaterRepository.findById(theaterNumber)
+                    .orElseThrow(() -> new NotFoundException(
+                        TheaterExceptionCode.THEATER_NOT_FOUND, 
+                        String.format(
+                            TheaterExceptionCode.THEATER_NOT_FOUND.getMessage(), 
+                            theaterNumber)));
+    }
+}
---
+++ src/test/java/com/uos/dsd/cinema/application/service/theater/TheaterServiceTest.java
@@ -0,0 +1,105 @@
+package com.uos.dsd.cinema.application.service.theater;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.uos.dsd.cinema.adaptor.in.web.theater.response.TheaterResponse;
+import com.uos.dsd.cinema.application.port.in.theater.command.CreateTheaterCommand;
+import com.uos.dsd.cinema.application.port.in.theater.command.ModifyTheaterCommand;
+import com.uos.dsd.cinema.application.service.TheaterService;
+import com.uos.dsd.cinema.application.port.out.theater.TheaterRepository;
+import com.uos.dsd.cinema.domain.theater.Theater;
+import com.uos.dsd.cinema.domain.theater.TheaterFixture;
+import com.uos.dsd.cinema.domain.theater.enums.LayoutElement;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+import java.util.List;
+
+@SpringBootTest
+@RequiredArgsConstructor
+@DisplayNameGeneration(ReplaceUnderscores.class)
+public class TheaterServiceTest {
+
+    private final TheaterService theaterService;
+    private final TheaterRepository theaterRepository;
+
+    private final Long theaterNumber = TheaterFixture.getTheaterNumber();
+    private final String theaterName = TheaterFixture.getTheaterName();
+    private final List<List<LayoutElement>> layout = TheaterFixture.getLayout();
+    private final List<String> screenTypes = TheaterFixture.getScreenTypes();
+    private final Theater theater = TheaterFixture.getTheater();
+    private final CreateTheaterCommand createTheaterCommand = TheaterFixture.getCreateTheaterCommand();
+    private final ModifyTheaterCommand modifyTheaterCommand = TheaterFixture.getModifyTheaterCommand();
+
+    @Test
+    public void testCreateTheater() {
+
+        // given
+        when(theaterRepository.save(theater))
+                .thenReturn(theater);
+
+        // when
+        Long createdTheaterNumber = theaterService.createTheater(createTheaterCommand);
+        
+        // then
+        assertEquals(theaterNumber, createdTheaterNumber);
+    }
+
+    @Test
+    public void testModifyTheater() {
+
+        // given
+        when(theaterRepository.findById(theaterNumber))
+                .thenReturn(Optional.of(theater));
+        when(theaterRepository.save(theater))
+                .thenReturn(theater);
+        when(theaterRepository.saveAndFlush(theater))
+                .thenReturn(theater);
+
+        // when
+        theaterService.modifyTheater(modifyTheaterCommand);
+
+        // then
+        assertEquals(theaterNumber, theater.getNumber());
+    }
+
+    @Test
+    public void testDeleteTheater() {
+
+        // given
+        doNothing().when(theaterRepository).deleteById(theaterNumber);
+
+        // when
+        theaterService.deleteTheater(theaterNumber);
+
+        // then
+        verify(theaterRepository, times(1)).deleteById(theaterNumber);
+    }
+
+    @Test
+    public void testReadTheater() {
+
+        // given
+        when(theaterRepository.findById(theaterNumber))
+                .thenReturn(Optional.of(theater));
+
+        // when
+        TheaterResponse foundTheater = theaterService.readTheater(theaterNumber);
+
+        // then
+        assertEquals(theaterNumber, foundTheater.number());
+        assertEquals(theaterName, foundTheater.name());
+        assertEquals(layout, foundTheater.layout());
+        assertEquals(screenTypes, foundTheater.screenTypes());
+    }
+}

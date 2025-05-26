e68e5f4973c2f2794461a80529ef327bac79c50e
ikjeong <jipark7937@gmail.com>
2025-05-27 02:34:47 +0900

    feat(Guest): Add guest entity

---
+++ src/main/java/com/uos/dsd/cinema/domain/customer/Customer.java
@@ -0,0 +1,48 @@
+package com.uos.dsd.cinema.domain.customer;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class Customer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType userType;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    protected LocalDateTime createdAt;
+    
+    protected Customer(UserType userType) {
+
+        if (userType == null) {
+            throw new IllegalArgumentException("User type is required");
+        }
+        this.userType = userType;
+    }
+}
---
+++ src/main/java/com/uos/dsd/cinema/domain/customer/UserType.java
@@ -0,0 +1,6 @@
+package com.uos.dsd.cinema.domain.customer;
+
+public enum UserType {
+    MEMBER, 
+    GUEST
+} 

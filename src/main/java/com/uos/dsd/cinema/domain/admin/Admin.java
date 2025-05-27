6c042e4f112ac109c98c3af209922cb3223fd1dc
ikjeong <jipark7937@gmail.com>
2025-05-27 21:03:41 +0900

    refactor(Admin): Add jpa constraints for username and password

--- src/main/java/com/uos/dsd/cinema/domain/admin/Admin.java
+++ src/main/java/com/uos/dsd/cinema/domain/admin/Admin.java
@@ -9,6 +9,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,7 +25,11 @@ public class Admin extends Base {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     public Admin(String username, String password) {

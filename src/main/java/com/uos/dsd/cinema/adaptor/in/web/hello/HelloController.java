be01d13dbcadc142f1b9f85c6df4596165c082ad
김혜주 <sueelly@naver.com>
2025-06-01 00:54:59 +0900

    feat(Hello): Add hello with authentication test

--- src/main/java/com/uos/dsd/cinema/adaptor/in/web/hello/HelloController.java
+++ src/main/java/com/uos/dsd/cinema/adaptor/in/web/hello/HelloController.java
@@ -1,8 +1,9 @@
 package com.uos.dsd.cinema.adaptor.in.web.hello;
 
 import com.uos.dsd.cinema.common.response.ApiResponse;
-
-import lombok.extern.slf4j.Slf4j;
+import com.uos.dsd.cinema.core.annotation.UserId;
+import com.uos.dsd.cinema.core.annotation.UserRole;
+import com.uos.dsd.cinema.core.security.SecurityConstants.Role;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/hello")
-@Slf4j
 public class HelloController {
 
     @GetMapping
     public ApiResponse<String> hello() {
-        log.info("hello api response");
         return ApiResponse.success("Hello, UOS Cinema!");
+    }
+
+    @GetMapping("/auth")
+    public ApiResponse<String> helloAuth(@UserId Long userId, @UserRole Role role) {
+        return ApiResponse.success("Hello, UOS Cinema! " + userId + " " + role.name());
     }
 }
--- src/test/java/com/uos/dsd/cinema/acceptance/hello/HelloAcceptanceTest.java
+++ src/test/java/com/uos/dsd/cinema/acceptance/hello/HelloAcceptanceTest.java
@@ -3,15 +3,20 @@ package com.uos.dsd.cinema.acceptance.hello;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import com.uos.dsd.cinema.acceptance.AcceptanceTest;
+import com.uos.dsd.cinema.acceptance.admin.steps.AdminSteps;
 import com.uos.dsd.cinema.acceptance.hello.steps.HelloSteps;
+import com.uos.dsd.cinema.adaptor.in.web.admin.request.AdminLoginRequest;
 import com.uos.dsd.cinema.common.response.ApiResponse;
 import com.uos.dsd.cinema.common.exception.code.CommonResultCode;
+import com.uos.dsd.cinema.utils.AuthHeaderProvider;
 
 import io.restassured.response.Response;
 import io.restassured.common.mapper.TypeRef;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+
+import java.util.Map;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 public class HelloAcceptanceTest extends AcceptanceTest {
@@ -31,5 +36,33 @@ public class HelloAcceptanceTest extends AcceptanceTest {
         assertEquals(CommonResultCode.SUCCESS.getMessage(), apiResponse.message());
         // data: Hello, UOS Cinema!
         assertEquals("Hello, UOS Cinema!", apiResponse.data());
+    }
+
+    @Test
+    public void helloAuth() {
+
+        // given
+        Long userId = -1L;
+        String username = "administrator";
+        String password = "password123!";
+        AdminLoginRequest request = new AdminLoginRequest(username, password);
+
+        Response loginResponse = AdminSteps.sendLoginAdmin(AuthHeaderProvider.createEmptyHeader(), request);
+        String accessToken = loginResponse.jsonPath().getString("data.accessToken");
+        Map<String, Object> headers = AuthHeaderProvider.createAuthorizationHeader(accessToken);
+
+        // when
+        Response response = HelloSteps.getHelloAuth(headers);
+        log.info("response: {}", response.asString());
+
+        // then
+        ApiResponse<String> apiResponse = response.as(new TypeRef<ApiResponse<String>>() {});
+        assertEquals(200, response.statusCode());
+        // code: COM000
+        assertEquals(CommonResultCode.SUCCESS.getCode(), apiResponse.code());
+        // message: Success
+        assertEquals(CommonResultCode.SUCCESS.getMessage(), apiResponse.message());
+        // data: Hello, UOS Cinema!
+        assertEquals("Hello, UOS Cinema! " + userId + " ADMIN", apiResponse.data());
     }
 }

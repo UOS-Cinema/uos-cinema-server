5a746a5a3d1b806fcb0b7ce2a446a0fa48ff96b4
ikjeong <jipark7937@gmail.com>
2025-06-20 03:54:52 +0900

    feat(Actor): Add Actor entity

---
+++ src/main/java/com/uos/dsd/cinema/domain/actor/Actor.java
@@ -0,0 +1,40 @@
+package com.uos.dsd.cinema.domain.actor;
+
+import com.uos.dsd.cinema.common.model.Base;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "actors")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Actor extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    private String photoUrl;
+
+    public Actor(String name, String photoUrl) {
+        
+        this.name = name;
+        this.photoUrl = photoUrl;
+    }
+
+    public Actor(String name) {
+        this(name, null);
+    }
+}

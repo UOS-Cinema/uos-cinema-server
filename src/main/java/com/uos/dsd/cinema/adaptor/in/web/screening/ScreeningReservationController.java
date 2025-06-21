c36cb08d3c94aa039bc872eeb8c03eeb4b8cf056
김혜주 <sueelly@naver.com>
2025-06-22 07:52:38 +0900

    feat: Create get screening theater layout ui

--- src/main/java/com/uos/dsd/cinema/adaptor/in/web/screening/ScreeningReservationController.java
+++ src/main/java/com/uos/dsd/cinema/adaptor/in/web/screening/ScreeningReservationController.java
@@ -1,7 +1,5 @@
 package com.uos.dsd.cinema.adaptor.in.web.screening;
 
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.uos.dsd.cinema.domain.theater.enums.LayoutElement;
-import com.uos.dsd.cinema.application.port.out.screening.ScreeningRepository;
 import com.uos.dsd.cinema.common.response.ApiResponse;
+import com.uos.dsd.cinema.application.port.in.screening.usecase.GetScreeningTheaterLayoutUseCase;
+import com.uos.dsd.cinema.domain.theater.enums.LayoutElement;
 
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/screenings/{id}/reservations")
 public class ScreeningReservationController {
 
-    private final ScreeningRepository screeningRepository;
+    private final GetScreeningTheaterLayoutUseCase getScreeningTheaterLayoutUseCase;
 
     @GetMapping
     public ApiResponse<List<List<LayoutElement>>> getSeatingStatus(@PathVariable Long id) {
 
-        return ApiResponse.success(screeningRepository.getSeatingStatus(id));
+        return ApiResponse.success(
+                getScreeningTheaterLayoutUseCase.getScreeningTheaterLayout(id));
     }
 }

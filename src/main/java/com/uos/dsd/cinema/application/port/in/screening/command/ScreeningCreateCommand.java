c7f6c69a48d879e4a4f21fd7fda7300755165155
김혜주 <sueelly@naver.com>
2025-06-22 08:16:11 +0900

    refactor: Use string of ScreenType in command and request

--- src/main/java/com/uos/dsd/cinema/application/port/in/screening/command/ScreeningCreateCommand.java
+++ src/main/java/com/uos/dsd/cinema/application/port/in/screening/command/ScreeningCreateCommand.java
@@ -1,13 +1,11 @@
 package com.uos.dsd.cinema.application.port.in.screening.command;
 
-import com.uos.dsd.cinema.domain.screen_type.ScreenType;
-
 import java.time.LocalDateTime;
 
 public record ScreeningCreateCommand(
     Long movieId,
     Long theaterId,
-    ScreenType screenType,
+    String screenType,
     LocalDateTime startTime
 ) {
 }

9bfd0db66b80240b8b3ac8a189d264fc5572d87e
김혜주 <sueelly@naver.com>
2025-06-22 19:27:51 +0900

    feat: Implement MovieRepository

--- src/main/java/com/uos/dsd/cinema/adaptor/out/persistence/movie/MovieJpaRepository.java
+++ src/main/java/com/uos/dsd/cinema/adaptor/out/persistence/movie/MovieJpaRepository.java
@@ -1,11 +1,26 @@
 package com.uos.dsd.cinema.adaptor.out.persistence.movie;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.uos.dsd.cinema.application.port.out.movie.MovieRepository;
 import com.uos.dsd.cinema.domain.movie.Movie;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 @Repository
-public interface MovieJpaRepository extends JpaRepository<Movie, Long>, MovieRepository {
+public interface MovieJpaRepository
+        extends 
+    JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie>, MovieRepository {
+
+    @Query("""
+        SELECT m
+        FROM Movie m
+        JOIN FETCH m.director d
+        LEFT JOIN FETCH m.movieCasts mc
+        LEFT JOIN FETCH mc.actor a
+        LEFT JOIN FETCH m.genres g
+        WHERE m.id = :id
+    """)
+    Movie findWithDetail(Long id);
 }
--- src/main/java/com/uos/dsd/cinema/application/port/out/movie/MovieRepository.java
+++ src/main/java/com/uos/dsd/cinema/application/port/out/movie/MovieRepository.java
@@ -2,9 +2,19 @@ package com.uos.dsd.cinema.application.port.out.movie;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
 import com.uos.dsd.cinema.domain.movie.Movie;
 
 public interface MovieRepository {
 
+    Movie save(Movie movie);
+
     Optional<Movie> findById(Long id);
+
+    Page<Movie> findAll(Specification<Movie> spec, Pageable pageable);
+
+    Movie findWithDetail(Long id);
 }

b4698a62ed48eddc46cb088acdef44b6f860b5cb
HJ Kim <sueelly@naver.com>
2025-06-24 03:03:26 +0900

    fix(PaymentJpaRepository): get completed payment by querying not null approvedAT

--- src/main/java/com/uos/dsd/cinema/adaptor/out/persistence/payment/PaymentJpaRepository.java
+++ src/main/java/com/uos/dsd/cinema/adaptor/out/persistence/payment/PaymentJpaRepository.java
@@ -10,7 +10,7 @@ import com.uos.dsd.cinema.domain.payment.Payment;
 
 public interface PaymentJpaRepository extends JpaRepository<Payment, Long> {
 
-    @Query("SELECT p FROM Payment p WHERE p.reservationId = :reservationId AND p.status = 'COMPLETED'")
+    @Query("SELECT p FROM Payment p WHERE p.reservationId = :reservationId AND p.approvedAt IS NOT NULL")
     Optional<Payment> findByReservationId(@Param("reservationId") Long reservationId);
 }
 
